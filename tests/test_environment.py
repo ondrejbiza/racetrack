050182a480cd229e6615651b8d7f1a27b6284f80
Ondrej Biza <ondrej.biza@gmail.com>
2018-06-09 20:54:47 +0200

    correct strict racetrack unit tests

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,31 +1,32 @@
 import unittest
-import environment, racetracks
+import racetracks
+from environment import RacetrackStrict, Racetrack
 
 
-class TestEnvironment(unittest.TestCase):
+class TestRacetrackStrict(unittest.TestCase):
 
   def test_load_racetrack(self):
 
-    environment.Racetrack(racetracks.TRACK_1)
-    environment.Racetrack(racetracks.TRACK_2)
+    RacetrackStrict(racetracks.TRACK_1)
+    RacetrackStrict(racetracks.TRACK_2)
 
   def test_correct_route(self):
 
-    env = environment.Racetrack(racetracks.TRACK_1)
+    env = RacetrackStrict(racetracks.TRACK_1)
 
-    self.assertEqual(env.act(1, 0), -1)
+    self.assertEqual(env.act(1, 0), RacetrackStrict.STEP_REWARD)
 
     for _ in range(27):
-      self.assertEqual(env.act(0, 0), -1)
+      self.assertEqual(env.act(0, 0), RacetrackStrict.STEP_REWARD)
 
-    self.assertEqual(env.act(-1, 1), -1)
+    self.assertEqual(env.act(-1, 1), RacetrackStrict.STEP_REWARD)
 
     while not env.done:
-      self.assertEqual(env.act(0, 1), -1)
+      self.assertEqual(env.act(0, 1), RacetrackStrict.STEP_REWARD)
 
   def test_collision(self):
 
-    env = environment.Racetrack(racetracks.TRACK_1)
+    env = RacetrackStrict(racetracks.TRACK_1)
 
     reward = env.act(1, 1)
     while reward != env.OUT_OF_BOUNDS_REWARD:
@@ -33,7 +34,7 @@ class TestEnvironment(unittest.TestCase):
 
   def test_check_finish(self):
 
-    env = environment.Racetrack(racetracks.TRACK_1)
+    env = RacetrackStrict(racetracks.TRACK_1)
 
     for i in range(5):
       for j in range(3):
